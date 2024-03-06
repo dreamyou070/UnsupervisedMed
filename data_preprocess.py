c90b5dbc2e907555865dcb8a5dfe51727809a396
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-06 14:18:57 +0900

    data preprocess

--- data_preprocess.py
+++ data_preprocess.py
@@ -45,7 +45,9 @@ def main(args) :
                 sub_gz_dir = os.path.join(folder_dir, sub_gz)
                 for i in range(40, 100) :
                     pil = check_img(sub_gz_dir, trg_axis = i)
-                    pil.save(os.path.join(rgb_dir, f'{folder}_{i}.png'))
+                    save_dir = os.path.join(rgb_dir, f'{folder}_{i}.png')
+                    print(f'save_dir = {save_dir}')
+                    pil.save(save_dir)
 
 
 
